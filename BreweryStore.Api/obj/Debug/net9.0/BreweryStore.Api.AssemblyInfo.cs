ba73cd386c5285dba23f8a815cded8c5e3a4009c
furiax <carlmalfliet@proximus.be>
2025-03-22 21:07:16 +0100

    added loglevel to not show entityframeworkcore.database messages

--- BreweryStore.Api/obj/Debug/net9.0/BreweryStore.Api.AssemblyInfo.cs
+++ BreweryStore.Api/obj/Debug/net9.0/BreweryStore.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BreweryStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93fcfa5de3374246f6afa5520c32047d6967fc4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c5a524f8a982be9ccd7202bf4deb41ebabd0ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("BreweryStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BreweryStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
